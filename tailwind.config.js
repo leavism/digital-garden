e7fd17eae2e021ae826d1d27d5eeb0e5f36d4526
Huy Dang <ghuydang@gmail.com>
2025-10-19 15:16:09 -0700

    style(ui): reduce space of headings and lists

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,120 +6,232 @@ export default {
 				DEFAULT: {
 					css: {
 						h1: {
-							marginTop: "0",
-							marginBottom: "0.5em",
+							marginTop: "0.9em",
+							marginBottom: "0.8em",
 						},
 						h2: {
-							marginTop: "0",
-							marginBottom: "0.4em",
+							marginTop: "1em",
+							marginBottom: "0.5em",
+							FontSize: "1.8em",
 						},
 						h3: {
-							marginTop: "1em",
+							marginTop: "0.7em",
 							marginBottom: "0.3em",
 						},
 						h4: {
-							marginTop: "0.8em",
+							marginTop: "0.6em",
 							marginBottom: "0.25em",
 						},
 						p: {
 							marginTop: "0",
+							lineHeight: "1.6",
 							marginBottom: "0.75em",
+						},
+						code: {
+							"&::before": {
+								content: '""',
+							},
+							"&::after": {
+								content: '""',
+							},
+						},
+						"ul > li": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
+						},
+						"ol > li": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
+						},
+						"li > p": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
 						},
 					},
 				},
 				sm: {
 					css: {
 						h1: {
-							marginTop: "0",
-							marginBottom: "0.5em",
+							marginTop: "0.9em",
+							marginBottom: "0.8em",
 						},
 						h2: {
-							marginTop: "0",
-							marginBottom: "0.4em",
+							marginTop: "1em",
+							marginBottom: "0.5em",
+							FontSize: "1.8em",
 						},
 						h3: {
-							marginTop: "1em",
+							marginTop: "0.7em",
 							marginBottom: "0.3em",
 						},
 						h4: {
-							marginTop: "0.8em",
+							marginTop: "0.6em",
 							marginBottom: "0.25em",
 						},
 						p: {
 							marginTop: "0",
+							lineHeight: "1.6",
 							marginBottom: "0.75em",
+						},
+						code: {
+							"&::before": {
+								content: '""',
+							},
+							"&::after": {
+								content: '""',
+							},
+						},
+						ul: { marginTop: "0.9em", marginBottom: "0.9em" },
+						"ul > li": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
+						},
+						"ol > li": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
+						},
+						"li > p": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
 						},
 					},
 				},
 				base: {
 					css: {
 						h1: {
-							marginTop: "0",
-							marginBottom: "0.5em",
+							marginTop: "0.9em",
+							marginBottom: "0.8em",
 						},
 						h2: {
-							marginTop: "0",
-							marginBottom: "0.4em",
+							marginTop: "1em",
+							marginBottom: "0.5em",
+							FontSize: "1.8em",
 						},
 						h3: {
-							marginTop: "1em",
+							marginTop: "0.7em",
 							marginBottom: "0.3em",
 						},
 						h4: {
-							marginTop: "0.8em",
+							marginTop: "0.6em",
 							marginBottom: "0.25em",
 						},
 						p: {
 							marginTop: "0",
+							lineHeight: "1.6",
 							marginBottom: "0.75em",
+						},
+						code: {
+							"&::before": {
+								content: '""',
+							},
+							"&::after": {
+								content: '""',
+							},
+						},
+						"ul > li": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
+						},
+						"ol > li": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
+						},
+						"li > p": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
 						},
 					},
 				},
 				lg: {
 					css: {
 						h1: {
-							marginTop: "0",
-							marginBottom: "0.5em",
+							marginTop: "0.9em",
+							marginBottom: "0.8em",
 						},
 						h2: {
-							marginTop: "0",
-							marginBottom: "0.4em",
+							marginTop: "1em",
+							marginBottom: "0.5em",
+							FontSize: "1.8em",
 						},
 						h3: {
-							marginTop: "1em",
+							marginTop: "0.7em",
 							marginBottom: "0.3em",
 						},
 						h4: {
-							marginTop: "0.8em",
+							marginTop: "0.6em",
 							marginBottom: "0.25em",
 						},
 						p: {
 							marginTop: "0",
 							marginBottom: "0.75em",
+						},
+						code: {
+							"&::before": {
+								content: '""',
+							},
+							"&::after": {
+								content: '""',
+							},
+						},
+						"ul > li": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
+						},
+						"ol > li": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
+						},
+						"li > p": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
 						},
 					},
 				},
 				xl: {
 					css: {
 						h1: {
-							marginTop: "0",
-							marginBottom: "0.5em",
+							marginTop: "0.9em",
+							marginBottom: "0.8em",
 						},
 						h2: {
-							marginTop: "0",
+							marginTop: "0.9em",
 							marginBottom: "0.4em",
+							FontSize: "1.8em",
 						},
 						h3: {
-							marginTop: "1em",
+							marginTop: "0.7em",
 							marginBottom: "0.3em",
 						},
 						h4: {
-							marginTop: "0.8em",
+							marginTop: "0.6em",
 							marginBottom: "0.25em",
 						},
 						p: {
 							marginTop: "0",
+							lineHeight: "1.6",
 							marginBottom: "0.75em",
+						},
+						code: {
+							"&::before": {
+								content: '""',
+							},
+							"&::after": {
+								content: '""',
+							},
+						},
+						ul: { marginTop: "0.9em", marginBottom: "0.8em" },
+						ol: { marginTop: "0.9em", marginBottom: "0.8em" },
+						"ul > li": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
+						},
+						"ol > li": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
+						},
+						"li > p": {
+							marginTop: "0.25em",
+							marginBottom: "0.25em",
 						},
 					},
 				},
